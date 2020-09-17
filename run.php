e2668b5028e2efc3092c6e114d3bdb6002c22752
pajaR_19 <53967072+pajaar@users.noreply.github.com>
2020-09-17 13:48:22 +0700

    Update run.php

--- run.php
+++ run.php
@@ -1,7 +1,7 @@
 <?php
 //github.com/pajaar
 
-$file = "pajaar1337.jpg";
+$file = "image.png";
 $lol = file_get_contents($file);
 $result = 'data:image/'.pathinfo($file, PATHINFO_EXTENSION).';base64,'.base64_encode($lol);
 echo $result;
